ee66742c8db104c58c3c699ed492010ec8a8f3e8
man2720 <aymanzabero@gmail.com>
2025-06-07 13:16:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.title("--Haii--")
 st.write("Tugas Informatika Jonathan .")
+
 st.image("wp-1625212579138.png",width=500)
 st.title("Aplikasi Sederhana")
 st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
